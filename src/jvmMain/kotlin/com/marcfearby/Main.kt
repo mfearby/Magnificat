615431aa0de355b89be678c427e9339a2b40b9bb
Marc Fearby <marc.fearby@service.nsw.gov.au>
2023-01-28 10:55:52 +1100

    Set window position and size; move App to separate file

--- src/jvmMain/kotlin/com/marcfearby/Main.kt
+++ src/jvmMain/kotlin/com/marcfearby/Main.kt
@@ -1,34 +1,24 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.marcfearby
 
-import androidx.compose.material.MaterialTheme
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
-
-@Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Magnificat!"
-        }) {
-            Text(text)
-        }
-    }
-}
+import androidx.compose.ui.window.rememberWindowState
+import com.marcfearby.view.App
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Magnificat",
+        state = rememberWindowState(
+            width = 1100.dp,
+            height = 750.dp,
+            position = WindowPosition(Alignment.Center)
+        )
+    ) {
         App()
     }
 }
---
+++ src/jvmMain/kotlin/com/marcfearby/view/App.kt
@@ -0,0 +1,21 @@
+package com.marcfearby.view
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+
+@Composable
+@Preview
+fun App() {
+    var text by remember { mutableStateOf("Hello, World!") }
+
+    MaterialTheme {
+        Button(onClick = {
+            text = "Hello, Magnificat!"
+        }) {
+            Text(text)
+        }
+    }
+}
