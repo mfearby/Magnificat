52b23bf007510a4aff4c0bb3242816b9570d421c
Marc Fearby <marc.fearby@service.nsw.gov.au>
2023-06-04 10:51:51 +1000

    Initialise Koin before the application starts
    I am now getting this error unless I start it explicitly before the application: "java.lang.IllegalStateException: KoinApplication has not been started"; must have been lucky to avoid this previously

--- src/main/kotlin/com/marcfearby/Main.kt
+++ src/main/kotlin/com/marcfearby/Main.kt
@@ -1,13 +1,17 @@
 package com.marcfearby
 
 import androidx.compose.ui.window.application
+import com.marcfearby.common.di.initKoin
 import com.marcfearby.view.App
 import com.marcfearby.view.MainWindow
 
-fun main() = application {
-    MainWindow(
-        onClose = ::exitApplication
-    ) {
-        App()
+fun main() {
+    initKoin()
+    application {
+        MainWindow(
+            onClose = ::exitApplication
+        ) {
+            App()
+        }
     }
 }
